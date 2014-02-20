4e8d7f2dc5608a37b7c5be3692dc252ba46d16db
Jesper Dramsch <jesper.dramsch@zmaw.de>
2014-02-20 13:02:16 +0100

    Generator working

--- A014.py
+++ A014.py
@@ -1,5 +1,8 @@
-import numpy as np
+# -*- coding: utf-8 -*-
+import time as t
+start = t.time()
 
+"""
 def collatz(n):
     out=[n]
     while not n==1:
@@ -16,6 +19,25 @@ for k in xrange(1,10**6+1):
     if col > maxi:
         maxi=col
         act=k
-        print(k)
         
 print act
+"""
+
+def collatz(n):
+    while not n == 1:
+        n=int(n*.5 if n&1==0 else 3*n+1)
+	yield n
+
+maxi=0
+for k in xrange(1,10**6+1):
+    col=sum(1 for l in collatz(k))
+    if col > maxi:
+	maxi=col
+        act=k
+        print k
+        
+print act
+
+
+end = t.time()
+print end-start
