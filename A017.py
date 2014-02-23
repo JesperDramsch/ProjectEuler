4aa135ea63dd30634ff2269e447d2cab54ee1c4a
JesperDramsch <jesper.dramsch@zmaw.de>
2014-02-23 19:16:19 +0100

    Start Dictionary for numbers

---
+++ A017.py
@@ -0,0 +1,7 @@
+import numpy as np
+import operator
+
+einer = {1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine"}
+zehner = {1:"Ten",2:"Twenty",3:"Thirty",4:"Forty",5:"Fivty",6:"Sixty",7:"Seventy",8:"Eighty",9:"Ninety"}
+zehnt = {11:"Eleven",12:"Twelve",13:"Thriteen",14:"Fourteen",15:"Fivteen",16:"Sixteen",17:"Seventeen",18:"Eighteen",19:"Nineteen"}
+
